5fcedf61876853872f4f83a4bac75c779565faf2
AakashJai06 <127650255+AakashJai06@users.noreply.github.com>
2024-06-01 13:46:54 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -12,7 +12,7 @@ struct Process{
 void sort(struct Process processes[],int n){
     int current = processes[0].arrival;
     
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         int min = i;
         for(int j=i+1;j<n;j++){
             if(processes[j].burst < processes[min].burst && processes[j].arrival <= current){
