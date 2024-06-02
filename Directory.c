200284d2fafb8ad513e49c8ffb64dd5a3ecbcc76
AakashJai06 <127650255+AakashJai06@users.noreply.github.com>
2024-06-02 18:58:10 +0530

    Create Directory.c

---
+++ Directory.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <dirent.h>
+
+struct dirent *dptr;
+
+int main(){
+    
+    char dirName[50];
+    
+    DIR *dirp;
+    
+    printf("Enter Directory Name--------->");
+    scanf("%s",dirName);
+    
+    if(!(dirp = opendir(dirName))){
+        printf("<------Error------>");
+        return 1;
+    }
+    
+    while(dptr == readdir(dirp)){
+        printf("%s\n", dptr->d_name);
+    }
+    closedir(dirp);
+    return 0;
+}
