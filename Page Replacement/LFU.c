c39614718431d2825fe9052f3b763dc7df716260
AakashJai06 <127650255+AakashJai06@users.noreply.github.com>
2024-07-08 13:30:30 +0530

    Update LFU.c

--- Page Replacement/LFU.c
+++ Page Replacement/LFU.c
@@ -1,67 +1,61 @@
 #include <stdio.h>
 
-int main() {
-    int n, rf[20], f, flag[20] = {0},m[20], freq[20] = {0}, time[20] = {0};
-    int i, j, pf = 0, next = 0, minFreq, minTime, minIndex;
+int main(){
+    
+    int f[10],m[50],count =0,n,fn,flag =0,next=0;
 
-    printf("Enter the length of the reference string: ");
-    scanf("%d", &n);
+printf("Number of pages-->");
+scanf("%d",&n);
 
-    printf("Enter the reference string: ");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &rf[i]);
+printf("page numbers -->");
+for(int i=0;i<n;i++)
+    scanf("%d",&m[i]);
+
+printf("Number of frames -->");
+scanf("%d",&fn);
+int current[fn],freq[fn];
+for(int i=0;i<fn;i++){
+    f[i] = -1;
+    current[i] = 0 ;  
+    freq[i] = 0; 
+}
+
+printf("Page repacement starts--\n");
+
+for(int i=0;i<n;i++){
+    flag =0;
+    for(int j=0;j<fn;j++){
+        if(f[j] == m[i]){
+            flag =1;
+            freq[j]++;
+            current[j] = next++;
+            break;
+        }
     }
-
-    printf("Enter the number of frames: ");
-    scanf("%d", &f);
-
-    for (i = 0; i < f; i++) {
-        m[i] = -1;
-    }
-
-    for (i = 0; i < n; i++) {
-        int found = 0;
-
-        for (j = 0; j < f; j++) {
-            if (m[j] == rf[i]) {
-                flag[i] = 1;
-                freq[j]++;
-                time[j] = next++;
-                found = 1;
-                break;
+    if(flag == 0){
+        if(next < fn){
+            f[next] = m[i];
+            current[next] = next;
+            freq[next++] = 1;
+        }
+        else{
+            int min =0;
+            for(int k=1;k<fn;k++){
+                if(freq[min] > freq[k] || (freq[min] == freq[k] && current[min] > current[k]))
+                min = k;
             }
+            f[min] = m[i];
+            current[min] = next++;
+            freq[min] = 1;
         }
-
-        if (!found) {
-            if (i < f) {
-                m[i] = rf[i];
-                freq[i] = 1;
-                time[i] = next++;
-            } else {
-                minIndex = 0;
-                minFreq = freq[0];
-                minTime = time[0];
-                for (j = 1; j < f; j++) {
-                    if (freq[j] < minFreq || (freq[j] == minFreq && time[j] < minTime)) {
-                        minIndex = j;
-                        minFreq = freq[j];
-                        minTime = time[j];
-                    }
-                }
-                m[minIndex] = rf[i];
-                freq[minIndex] = 1;
-                time[minIndex] = next++;
-            }
-            pf++;
-        }
-
-        for (j = 0; j < f; j++) {
-            printf("%d ", m[j]);
-        }
-        printf("\n");
     }
+    for(int i=0;i<fn;i++){
+        if(f[i]!=-1)
+            printf("%d\t",f[i]);
+    }
+    printf("\n");
+}
 
-    printf("Number of page faults is %d\n", pf);
 
-    return 0;
+  return 0;
 }
